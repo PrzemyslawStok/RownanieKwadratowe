52564d838527ecc9981a80d6e2bb6b4909002649
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-17 15:00:09 +0200

    cd..

--- src/RownanieKwadratowe.kt
+++ src/RownanieKwadratowe.kt
@@ -1,3 +1,20 @@
-class RownanieKwadratowe(val A:Double,val B:Double,val C:Double) {
-    
+class RownanieKwadratowe(val A: Double, val B: Double, val C: Double) {
+    val delta: Double;
+
+    fun obliczDelte(): Double {
+        return B * B - 4 * A * C;
+    }
+
+    fun deltaInfo(){
+        if(delta>0)
+            println("Delta jest większa od zero")
+        if(delta==0.0)
+            println("Delta wynosi zero")
+        if(delta<0)
+            println("Delta jest mniejsza od zero")
+    }
+
+    init {
+        delta = obliczDelte()
+    }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,5 @@
 fun main(){
     //5x^2+x+4;
-    val rownanieKwadratowe = RownanieKwadratowe(5.0,1.0,4.0);
+    val rownanieKwadratowe = RownanieKwadratowe(1.0,5.0,4.0)
+    rownanieKwadratowe.deltaInfo()
 }
