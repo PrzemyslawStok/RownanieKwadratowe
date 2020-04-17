eac66ce7d8746410f306d098f7a0d9f077fbe252
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-17 15:04:19 +0200

    cd..

--- src/RownanieKwadratowe.kt
+++ src/RownanieKwadratowe.kt
@@ -1,8 +1,19 @@
 class RownanieKwadratowe(val A: Double, val B: Double, val C: Double) {
     val delta: Double;
 
+    var x0 = 0.0;
+    var x1 = 0.0;
+
     fun obliczDelte(): Double {
         return B * B - 4 * A * C;
+    }
+
+    fun obliczDwaRozwiazania(){
+
+    }
+
+    fun obliczJednoRozwiazanie(){
+
     }
 
     fun deltaInfo(){
@@ -12,6 +23,15 @@ class RownanieKwadratowe(val A: Double, val B: Double, val C: Double) {
             println("Delta wynosi zero")
         if(delta<0)
             println("Delta jest mniejsza od zero")
+    }
+
+    fun oblicz(){
+        if(delta>0)
+            obliczDwaRozwiazania()
+        if(delta==0.0)
+            obliczJednoRozwiazanie()
+        if(delta<0)
+            return
     }
 
     init {
