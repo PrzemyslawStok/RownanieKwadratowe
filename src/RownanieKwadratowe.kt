d4d4df733161635fe3979a0755e064d3ddaa506e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-04-17 15:16:17 +0200

    cd..

--- src/RownanieKwadratowe.kt
+++ src/RownanieKwadratowe.kt
@@ -1,36 +1,41 @@
-class RownanieKwadratowe(val A: Double, val B: Double, val C: Double) {
+import kotlin.math.sqrt
+
+class RownanieKwadratowe(val a: Double, val b: Double, val c: Double) {
     val delta: Double
 
     var x0 = 0.0
     var x1 = 0.0
+    var x2 = 0.0
 
     fun obliczDelte(): Double {
-        return B * B - 4 * A * C
+        return b * b - 4 * a * c
     }
 
-    fun obliczDwaRozwiazania(){
+    fun obliczDwaRozwiazania() {
+        x1 = (-b + sqrt(delta)) / (2 * a)
+        x2 = (b + sqrt(delta)) / (2 * a)
+    }
+
+    fun obliczJednoRozwiazanie() {
+        x0 = -b / (2 * a)
 
     }
 
-    fun obliczJednoRozwiazanie(){
-
-    }
-
-    fun deltaInfo(){
-        if(delta>0)
+    fun deltaInfo() {
+        if (delta > 0)
             println("Delta jest większa od zero")
-        if(delta==0.0)
+        if (delta == 0.0)
             println("Delta wynosi zero")
-        if(delta<0)
+        if (delta < 0)
             println("Delta jest mniejsza od zero")
     }
 
-    fun oblicz(){
-        if(delta>0)
+    fun oblicz() {
+        if (delta > 0)
             obliczDwaRozwiazania()
-        if(delta==0.0)
+        if (delta == 0.0)
             obliczJednoRozwiazanie()
-        if(delta<0)
+        if (delta < 0)
             return
     }
 
